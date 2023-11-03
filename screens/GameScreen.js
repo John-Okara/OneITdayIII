3e8e78cac6c2cebf21f725b081eca80b3b144514
Johann Schwarz <schwarz_johann88@yahoo.de>
2023-11-03 09:29:57 +0100

    add Cali to GameScreen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 function GameScreen() {
   return (
     <View style={styles.backround}>
       <Text style={styles.infotext}>Game Screen</Text>
+      <Image style={styles.cali} source={require("../assets/Cali.png")} />
     </View>
   );
 }
@@ -18,5 +19,12 @@ const styles = StyleSheet.create({
   },
   infotext: {
     color: "white",
+  },
+  cali: {
+    width: 120,
+    height: 120,
+    marginTop: 10,
+    marginBottom: 50,
+    marginLeft: 280,
   },
 });
